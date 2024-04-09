1b05cc436e1693b7f8b485b89dfe8441edfc2068
Matthias Gielen <107040873+diGiusepp3@users.noreply.github.com>
2024-04-09 20:09:12 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    $pagetitle = "Webcrafters";
+    $keywords = ['Webcrafters', 'Digitale oplossingen', 'webdesign', 'Website laten maken', 'wat kost een website laten maken', 'goedkoop website laten maken'];
+    $description = "Versterk uw bedrijf met digitale oplossingen";
+    include $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+
+    include $_SERVER['DOCUMENT_ROOT'] . '/footer.php';
+
+
